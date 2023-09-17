7745e37e570c12d23d89bae28af3869a5dab937f
JaydenLee1116 <vv55adss@gmail.com>
2023-09-17 20:39:13 +0900

    💄 design: global style 설정 및 적용
    - theme 추가 예정

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { RouterProvider } from 'react-router-dom';
 
 import router from './router';
+import { GlobalStyles } from './styles/GlobalStyles.styled';
 
 const App = () => {
   return (
     <>
+      <GlobalStyles />
       <RouterProvider router={router} />
     </>
   );
