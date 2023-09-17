3bb098ddeacb523391a69cd8ec16651172828f78
JaydenLee1116 <vv55adss@gmail.com>
2023-09-17 20:43:06 +0900

    ✨ feat: react-query provider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import router from './router';
 import { GlobalStyles } from './styles/GlobalStyles.styled';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </>
   );
 };
