7c92be621c0dc2470be4adabda9d8680fba9a912
JaydenLee1116 <vv55adss@gmail.com>
2023-09-20 20:10:37 +0900

    ✨ feat: Pretendard 글꼴 적용 및 RecipePage 적용

--- src/pages/RecipePage/RecipePage.styled.ts
+++ src/pages/RecipePage/RecipePage.styled.ts
@@ -5,10 +5,27 @@ export const Container = styled.section`
   height: 100%;
   display: flex;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
 `;
 
 export const Header = styled.header`
   width: 100%;
+  height: fit-content;
+`;
+
+export const Main = styled.main`
+  width: 100%;
+  height: fit-content;
   display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 16px 0;
+  gap: 24px;
+`;
+
+export const Footer = styled.footer`
+  width: 100%;
+  height: fit-content;
 `;
--- src/pages/RecipePage/index.tsx
+++ src/pages/RecipePage/index.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 import * as S from './RecipePage.styled';
-import MainNavigationBar from '../../components/RecipePage/MainNavigationBar';
+import MainGlobalNavBar from '../../components/common/MainGlobalNavBar';
+import FootNavBar from '../../components/common/FootNavBar';
 
 const RecipePage = () => {
   return (
     <S.Container>
       <S.Header>
-        <MainNavigationBar />
+        <MainGlobalNavBar />
       </S.Header>
+      <S.Main>메인 컨텐츠</S.Main>
+      <S.Footer>
+        <FootNavBar />
+      </S.Footer>
     </S.Container>
   );
 };
