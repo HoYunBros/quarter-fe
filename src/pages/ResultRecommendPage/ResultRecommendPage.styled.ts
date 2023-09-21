19ded6a639576a3f53b4f2f3d3b0da22663c679c
JaydenLee1116 <vv55adss@gmail.com>
2023-09-21 17:32:44 +0900

    💄 design #9: ResultRecommendPage UI 구현

---
+++ src/pages/ResultRecommendPage/ResultRecommendPage.styled.ts
@@ -0,0 +1,32 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.section`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Header = styled.header`
+  width: 100%;
+  height: fit-content;
+`;
+
+export const Main = styled.main`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  padding: 32px 20px;
+  gap: 24px;
+`;
+
+export const Footer = styled.footer`
+  width: 100%;
+  height: fit-content;
+  padding: 0 16px 32px 16px;
+`;
--- src/pages/ResultRecommendPage/index.tsx
+++ src/pages/ResultRecommendPage/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import { routes } from '../../router';
+import SubGlobalNavBar from '../../components/SubGlobalNavBar';
+import FootButton from '../../components/FootButton';
+import Text from '../../components/Text';
+import { PROGRESS_BAR_WIDTH } from '../../constants';
+import * as S from './ResultRecommendPage.styled';
+
 const ResultRecommendPage = () => {
   return (
-    <section>
-      <h1>ResultRecommend Page</h1>
-    </section>
+    <S.Container>
+      <S.Header>
+        <SubGlobalNavBar backTo={routes.ingredientSelect} progressWidth={PROGRESS_BAR_WIDTH.END} />
+      </S.Header>
+      <S.Main>
+        <Text size="large">나만의 꿀조합을 찾았어요!</Text>
+      </S.Main>
+      <S.Footer>
+        <FootButton nextTo={routes.sizePick}>다시하기</FootButton>
+      </S.Footer>
+    </S.Container>
   );
 };
 
