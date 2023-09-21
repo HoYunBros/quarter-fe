c5f5db568368b10612f89cfef5f54c8ff4e84814
JaydenLee1116 <vv55adss@gmail.com>
2023-09-21 15:54:03 +0900

    ✨ feat: SizePickPage 구현
    - 추후 상태관리 추가

--- src/pages/SizePickPage/index.tsx
+++ src/pages/SizePickPage/index.tsx
@@ -1,10 +1,72 @@
 import React from 'react';
 
+import { routes } from '../../router';
+import SubGlobalNavBar from '../../components/SubGlobalNavBar';
+import Text from '../../components/Text';
+import SizeBlock from '../../components/SizeBlock';
+import FootButton from '../../components/FootButton';
+import * as S from './SizePickPage.styled';
+
 const SizePickPage = () => {
   return (
-    <section>
-      <h1>SizePick Page</h1>
-    </section>
+    <S.Container>
+      <S.Header>
+        <SubGlobalNavBar backTo={routes.home} />
+      </S.Header>
+      <S.Main>
+        <Text size="large">어떤 사이즈를 드시나요?</Text>
+        <S.SizePickContainer>
+          <Text size="medium">1가지 맛</Text>
+          <S.SizesWrapper>
+            <S.SizeWrapper>
+              <SizeBlock size="single" />
+              <Text size="small">싱글 레귤러</Text>
+            </S.SizeWrapper>
+            <S.SizeWrapper>
+              <SizeBlock size="single" />
+              <Text size="small">싱글킹</Text>
+            </S.SizeWrapper>
+          </S.SizesWrapper>
+        </S.SizePickContainer>
+        <S.SizePickContainer>
+          <Text size="medium">2가지 맛</Text>
+          <S.SizesWrapper>
+            <S.SizeWrapper>
+              <SizeBlock size="double" />
+              <Text size="small">더블주니어</Text>
+            </S.SizeWrapper>
+            <S.SizeWrapper>
+              <SizeBlock size="double" />
+              <Text size="small">더블레귤러</Text>
+            </S.SizeWrapper>
+          </S.SizesWrapper>
+        </S.SizePickContainer>
+        <S.SizePickContainer>
+          <Text size="medium">3가지 맛 이상</Text>
+          <S.SizesWrapper>
+            <S.SizeWrapper>
+              <SizeBlock size="pint" />
+              <Text size="small">파인트</Text>
+            </S.SizeWrapper>
+            <S.SizeWrapper>
+              <SizeBlock size="quart" />
+              <Text size="small">쿼터</Text>
+            </S.SizeWrapper>
+            <S.SizeWrapper>
+              <SizeBlock size="family" />
+              <Text size="small">패밀리</Text>
+            </S.SizeWrapper>
+            <S.SizeWrapper>
+              <SizeBlock size="half-gallon" />
+              <Text size="small">하프갤런</Text>
+            </S.SizeWrapper>
+          </S.SizesWrapper>
+        </S.SizePickContainer>
+      </S.Main>
+      <S.Footer>
+        <FootButton nextTo={routes.ingredientSelect}>다음</FootButton>
+      </S.Footer>
+    </S.Container>
   );
 };
 
