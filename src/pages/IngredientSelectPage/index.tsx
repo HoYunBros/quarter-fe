51fced81a28f4d00a40cfd0296c96e1a509343e4
JaydenLee1116 <vv55adss@gmail.com>
2023-09-21 17:30:40 +0900

    💄 design #8: IngredientSelectPage UI 구현

--- src/pages/IngredientSelectPage/index.tsx
+++ src/pages/IngredientSelectPage/index.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+import { routes } from '../../router';
+import SubGlobalNavBar from '../../components/SubGlobalNavBar';
+import FootButton from '../../components/FootButton';
+import Text from '../../components/Text';
+import { PROGRESS_BAR_WIDTH } from '../../constants';
+import * as S from './IngredientSelectPage.styled';
+
 const IngredientSelectPage = () => {
   return (
-    <section>
-      <h1>IngredientSelect Page</h1>
-    </section>
+    <S.Container>
+      <S.Header>
+        <SubGlobalNavBar backTo={routes.sizePick} progressWidth={PROGRESS_BAR_WIDTH.MIDDLE} />
+      </S.Header>
+      <S.Main>
+        <Text size="large">좋아하는 재료를 선택해주세요</Text>
+      </S.Main>
+      <S.Footer>
+        <FootButton nextTo={routes.resultRecommend}>결과보기</FootButton>
+      </S.Footer>
+    </S.Container>
   );
 };
 
