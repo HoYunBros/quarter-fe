765c065c2096d70c122c58cd50537f11844061ec
JaydenLee1116 <vv55adss@gmail.com>
2023-09-22 17:45:41 +0900

    ✨ feat #8: IngredientSelectPage 재료 데이터 렌더링 구현

--- src/pages/IngredientSelectPage/index.tsx
+++ src/pages/IngredientSelectPage/index.tsx
@@ -7,19 +7,37 @@ import Text from '../../components/Text';
 import { PROGRESS_BAR_WIDTH } from '../../constants';
 import * as S from './IngredientSelectPage.styled';
 
+import { useGetIngredients } from '../../services/useGetIngredients';
+
 const IngredientSelectPage = () => {
+  const { data, isLoading, isError } = useGetIngredients();
+
+  console.log(data, isLoading, isError);
   return (
-    <S.Container>
-      <S.Header>
-        <SubGlobalNavBar backTo={routes.sizePick} progressWidth={PROGRESS_BAR_WIDTH.MIDDLE} />
-      </S.Header>
-      <S.Main>
-        <Text size="large">좋아하는 재료를 선택해주세요</Text>
-      </S.Main>
-      <S.Footer>
-        <FootButton nextTo={routes.resultRecommend}>결과보기</FootButton>
-      </S.Footer>
-    </S.Container>
+    <>
+      {isLoading && <div>loading...</div>}
+      {data && (
+        <S.Container>
+          <S.Header>
+            <SubGlobalNavBar backTo={routes.sizePick} progressWidth={PROGRESS_BAR_WIDTH.MIDDLE} />
+          </S.Header>
+          <S.Main>
+            <Text size="large">좋아하는 재료를 선택해주세요</Text>
+            <S.IngredientsContainer>
+              {data?.ingredients.map(ingredient => (
+                <S.IngredientWrapper key={ingredient.id}>
+                  <S.IngredientImage src={ingredient.imageUrl} alt={ingredient.name} />
+                  <S.IngredientName>{ingredient.name}</S.IngredientName>
+                </S.IngredientWrapper>
+              ))}
+            </S.IngredientsContainer>
+          </S.Main>
+          <S.Footer>
+            <FootButton nextTo={routes.resultRecommend}>결과보기</FootButton>
+          </S.Footer>
+        </S.Container>
+      )}
+    </>
   );
 };
 
