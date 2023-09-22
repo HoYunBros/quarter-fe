1feb84ef6b6d9ac2c44dcf18b7012b127b6fcc5b
JaydenLee1116 <vv55adss@gmail.com>
2023-09-23 02:58:52 +0900

    💄 design #10: FootButton props 수정
    - onClick 추가

--- src/components/FootButton/index.tsx
+++ src/components/FootButton/index.tsx
@@ -1,20 +1,18 @@
 import React from 'react';
 
 import * as S from './FootButton.styled';
-import { useNavigate } from 'react-router-dom';
 
 type Props = {
   children: React.ReactNode;
-  nextTo: string;
+  onClick: () => void;
   disabled?: boolean;
 };
 
-const FootButton = ({ children, nextTo, disabled = true }: Props) => {
-  const navigate = useNavigate();
+const FootButton = ({ children, onClick, disabled = true }: Props) => {
   return (
     <S.Button
       onClick={() => {
-        navigate(nextTo);
+        onClick();
       }}
       disabled={disabled}
       $disabled={disabled}
