561e8805ecd753a4072d8a705a2db7373bc01751
JaydenLee1116 <vv55adss@gmail.com>
2023-09-22 22:48:36 +0900

    ✨ feat #10: FootButton disabled state 추가

--- src/components/FootButton/index.tsx
+++ src/components/FootButton/index.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 
 import * as S from './FootButton.styled';
+import { useNavigate } from 'react-router-dom';
 
 type Props = {
   children: React.ReactNode;
   nextTo: string;
+  disabled?: boolean;
 };
 
-const FootButton = ({ children, nextTo }: Props) => {
-  return <S.Button to={nextTo}>{children}</S.Button>;
+const FootButton = ({ children, nextTo, disabled = true }: Props) => {
+  const navigate = useNavigate();
+  return (
+    <S.Button
+      onClick={() => {
+        navigate(nextTo);
+      }}
+      disabled={disabled}
+      $disabled={disabled}
+    >
+      {children}
+    </S.Button>
+  );
 };
 
 export default FootButton;
