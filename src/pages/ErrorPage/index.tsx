640118510d6877f82870b866d171b5cc7283cac6
JaydenLee1116 <vv55adss@gmail.com>
2023-09-23 14:50:18 +0900

    ✨ feat #21: Error Page 구현

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { useMode } from '../../contexts/ModeContext';
+import { ReactComponent as NotYetLight } from '../../assets/icons/character/not_yet_light.svg';
+import { ReactComponent as NotYetDark } from '../../assets/icons/character/not_yet_dark.svg';
+import MainGlobalNavBar from '../../components/MainGlobalNavBar';
+import FootNavBar from '../../components/FootNavBar';
+import Text from '../../components/Text';
+import * as S from './ErrorPage.styled';
 
 const ErrorPage = () => {
+  const mode = useMode();
+  const navigate = useNavigate();
+  const initialCountdown = 3;
+  const [countdown, setCountdown] = useState(initialCountdown);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setCountdown(countdown - 1);
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+      if (countdown === 1) navigate('/');
+    };
+  }, [countdown]);
+
   return (
-    <section>
-      <h1>Error Page</h1>
-    </section>
+    <S.Container>
+      <S.Header>
+        <MainGlobalNavBar />
+      </S.Header>
+      <S.Main>
+        {mode === 'light' ? <NotYetLight /> : <NotYetDark />}
+        <Text size="medium" color="gray">
+          잘못된 접근이에요!
+        </Text>
+        <Text size="small" color="gray">
+          {countdown}초 후에 홈으로 이동합니다.
+        </Text>
+      </S.Main>
+      <S.Footer>
+        <FootNavBar />
+      </S.Footer>
+    </S.Container>
   );
 };
 
